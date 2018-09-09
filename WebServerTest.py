5d65500349503ed635a2f78cfe7310b56eb09288
cdbeveridge <43109502+cdbeveridge@users.noreply.github.com>
2018-09-09 12:00:54 +0100

    commit

---
+++ WebServerTest.py
@@ -0,0 +1,28 @@
+import urllib2
+import socket
+import sys
+
+
+#check for successful http request
+def check_url(url, timeout=5):
+
+    try:
+        return urllib2.urlopen(url,timeout=timeout).getcode() == 200
+    except urllib2.URLError as e:
+        return False
+    except socket.timeout as e:
+        print False
+
+#list of sites to check 
+def CheckSites():
+
+	sites = ["enter siteList here"]
+
+	for sites in sites:
+		print (sites), 'is available: ', check_url(sites)
+
+def Main():
+
+	CheckSites()
+
+Main()
